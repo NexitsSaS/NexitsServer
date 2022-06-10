29abdc8e1abf7fb72589b07c1ca65a0d0b34a58f
dhaoui aziz <dhaouiaziz13@gmail.com>
2022-06-10 17:54:22 +0100

    schema draft

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+//-------------------------this is a routes exemple------------------------//
+// const router = require("express").Router();
+// const authcontroller = require("../controllers/authcontroller");
+
+// router.post("/login", authcontroller.login);
+// router.post("/signup", authcontroller.Signup);
+// router.post("linkMail", authcontroller.linkmail);
+// router.post("/logout", authcontroller.logout);
+
+// module.exports = router;
